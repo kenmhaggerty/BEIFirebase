10fbbc0ab2309b39de13894365af1d6e78d99e3d
kenmhaggerty <kenmhaggerty@protonmail.com>
2017-06-19 15:55:42 -0400

    [IN PROGRESS] Attempting to get app to communicate with Firebase; adds jQuery; moves Firebase initialize into head in index.html.

--- firebase.js
+++ firebase.js
@@ -2,14 +2,21 @@
 // BEIFirebase
 // KEN M. HAGGERTY
 // CREATED : Jun 07 2017
-// EDITED  : Jun 07 2017
+// EDITED  : Jun 19 2017
 
 function createReview(product, rating, review, username, privacy) {
-	var path = 'products/' + product;
-	firebase.database().ref(path).push({
-		rating : rating,
-		review : review,
-		username : username,
-		privacy : privacy
-	});
+	// firebase.database().ref().child(product).push({
+	// 	rating : rating,
+	// 	review : review,
+	// 	username : username,
+	// 	privacy : privacy
+	// });
+	var key = firebase.database().ref().child("strings").push(product);
+	alert(product+", "+rating+", "+review+", "+username+", "+privacy+", "+key);
 }
+
+$("#submit").on("click", function(){
+  var $input = $("#product");
+  var key = firebase.database().ref().child("strings").push($input.val());
+  alert(key);
+});
